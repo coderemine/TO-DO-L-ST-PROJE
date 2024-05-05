38c0dedf07a7231908599f5cf72c6a8b1167eb4e
coderemine <160237384+coderemine@users.noreply.github.com>
2024-05-05 21:38:01 +0300

    Add files via upload

---
+++ deneme.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> _books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Author = "Author 1", Year = 2020 },
+            new Book { Id = 2, Title = "Book 2", Author = "Author 2", Year = 2021 },
+            new Book { Id = 3, Title = "Book 3", Author = "Author 3", Year = 2022 }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return _books;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(Book book)
+        {
+            book.Id = _books.Count + 1; // Yeni bir ID oluştur
+            _books.Add(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book book)
+        {
+            var existingBook = _books.FirstOrDefault(b => b.Id == id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            existingBook.Title = book.Title;
+            existingBook.Author = book.Author;
+            existingBook.Year = book.Year;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var existingBook = _books.FirstOrDefault(b => b.Id == id);
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+            _books.Remove(existingBook);
+            return NoContent();
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
